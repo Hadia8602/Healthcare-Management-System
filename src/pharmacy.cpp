92f389ba40994c0059733368ba16e375697614fa
Hadia8602 <hadiaishaq932@gmail.com>
2026-01-02 13:41:18 +0500

    Added view() function for pharmacy module

--- src/pharmacy.cpp
+++ src/pharmacy.cpp
@@ -4,21 +4,11 @@
 #include "pharmacy.h"
 using namespace std;
 
-void add() {
-    string amount, name, expiry, mg;
-    cout << "Enter name of new medicine: ";
-    getline(cin, name);
-    cout << "Enter the number of medicines: ";
-    getline(cin, amount);
-    cout << "Enter the expiry date: ";
-    getline(cin, expiry);
-    cout << "Enter the mg of medicine: ";
-    getline(cin, mg);
-
-    ofstream file("pharmacy.csv", ios::app);
-    file << amount << "," << name << "," << mg << "," << expiry << "\n";
+void view() {
+    string line;
+    ifstream file("pharmacy.csv", ios::in);
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
     file.close();
-    cout << "The medicine is added successfully\n";
 }
-
-
