eb83420bfd80aad509bce14d4f3e2535bd078988
Hadia8602 <hadiaishaq932@gmail.com>
2026-01-02 13:31:33 +0500

    Added add() function for pharmacy module

--- src/pharmacy.cpp
+++ src/pharmacy.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "pharmacy.h"
 using namespace std;
 
-void pharmacyMenu() {
-    cout << "Pharmacy Module Working\n";
+void add() {
+    string amount, name, expiry, mg;
+    cout << "Enter name of new medicine: ";
+    getline(cin, name);
+    cout << "Enter the number of medicines: ";
+    getline(cin, amount);
+    cout << "Enter the expiry date: ";
+    getline(cin, expiry);
+    cout << "Enter the mg of medicine: ";
+    getline(cin, mg);
+
+    ofstream file("pharmacy.csv", ios::app);
+    file << amount << "," << name << "," << mg << "," << expiry << "\n";
+    file.close();
+    cout << "The medicine is added successfully\n";
 }
+
 
