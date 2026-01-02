957c5bf138b56a70799aeefe35d970033e6f5407
Hadia8602 <hadiaishaq932@gmail.com>
2026-01-02 13:42:47 +0500

    Added view() function for pharmacy module

--- src/pharmacy.cpp
+++ src/pharmacy.cpp
@@ -4,11 +4,40 @@
 #include "pharmacy.h"
 using namespace std;
 
-void view() {
-    string line;
-    ifstream file("pharmacy.csv", ios::in);
+void edit() {
+    string line, noOfTablets, expiry_date, mg, name;
+    string nameToUpdate, expiry, amount;
+    cout << "Enter the name of medicine you want to edit\n";
+    getline(cin, nameToUpdate);
+    cout << "Enter the number of tablets\n";
+    getline(cin, amount);
+    cout << "Enter the new expiry date\n";
+    getline(cin, expiry);
+    cout << "Enter the new mg\n";
+    getline(cin, mg);
+
+    bool found = false;
+    ifstream file("pharmacy.csv");
+    ofstream temp("temp.csv");
     while (getline(file, line)) {
-        cout << line << endl;
+        stringstream ss(line);
+        getline(ss, noOfTablets, ',');
+        getline(ss, name, ',');
+        getline(ss, mg, ',');
+        getline(ss, expiry_date, ',');
+        if (nameToUpdate == name) {
+            temp << amount << "," << nameToUpdate << "," << mg << "," << expiry << "\n";
+            found = true;
+        } else {
+            temp << line << "\n";
+        }
     }
     file.close();
+    temp.close();
+    remove("pharmacy.csv");
+    rename("temp.csv", "pharmacy.csv");
+    if (found)
+        cout << "medicine updated successfully\n";
+    else
+        cout << "invalid name!\n";
 }
