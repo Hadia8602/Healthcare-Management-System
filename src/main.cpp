031c461562668ee988a3a4cb11d1ac86818a0b17
Hadia8602 <hadiaishaq932@gmail.com>
2026-01-02 16:29:49 +0500

    integrated staff menu with pharmacy system

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-#include "pharmacy.h"
-using namespace std;
-
-int main() {
-    pharmacyMenu(); 
-    return 0;
+switch(choice) {
+    case 1: pharmacyMenu(); break;
+    case 2: staffMenu(); break;        // <-- This is your Staff menu
+    case 3: /* billing menu */ break;
+    case 4: /* lab menu */ break;
+    case 5: /* patient menu */ break;
+    case 6: cout << "Exiting Program...\n"; break;
+    default: cout << "Invalid Choice\n";
 }
 
